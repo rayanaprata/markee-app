3341a6244583ef577ba777c94e73d2bafd894ad8
Rayana Prata <50561896+rayanaprata@users.noreply.github.com>
2021-09-19 21:59:04 -0300

    start sidebar

--- src/sidebar/sidebar.tsx
+++ src/sidebar/sidebar.tsx
@@ -1,8 +1,15 @@
 import * as S from './sidebar-styles'
+import * as I from 'icons'
 
 function Sidebar () {
   return (
-    <S.Sidebar>Sidebar</S.Sidebar>
+    <S.Sidebar>
+
+      <S.Button>
+        <I.PlusDark /> Adicionar arquivo
+      </S.Button>
+
+    </S.Sidebar>
   )
 }
 export { Sidebar }
